4a16ffdc526a345bca78f072159793cf5b6c8300
VinayParmar555 <vinayparmar111111@gmail.com>
2025-12-11 15:45:53 +0530

    Minor change

--- app/db/models/products.py
+++ app/db/models/products.py
@@ -3,7 +3,7 @@ from app.db.base import Base
 
 class Product(Base):
    
-    __tablename__ = "product"
+    __tablename__ = "products"
    
     id = Column(Integer, primary_key=True, index= True)
     name = Column(String)
