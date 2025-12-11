d5f48f42b265be2ad083063e058fd9b679235d9e
VinayParmar555 <vinayparmar111111@gmail.com>
2025-12-11 15:44:26 +0530

    Changed DB URL path

--- alembic/env.py
+++ alembic/env.py
@@ -1,14 +1,13 @@
 from logging.config import fileConfig
-
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
+from sqlalchemy import engine_from_config, pool
 from app.db.base import Base
-
+from app.config.settings import settings
 from alembic import context
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+config.set_main_option("sqlalchemy.url", settings.DATABASE_URL)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
