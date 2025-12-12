f1a909b58b58390be2fb2fb9635de94d438dd903
VinayParmar555 <vinayparmar111111@gmail.com>
2025-12-12 22:09:47 +0530

    Added seperate service/product_service.py for business logic

---
+++ app/services/product_service.py
@@ -0,0 +1,44 @@
+from sqlalchemy.orm import Session
+from app.db.models.products import Product
+from app.schema.products import ProductBase
+
+def List_of_products(db:Session):
+    
+    db_products = db.query(Product).all()
+    
+    return db_products
+
+def search_product(db:Session, id: int):
+    db_product = db.get(Product, id)
+    if not db_product:
+        return False
+    return db_product
+
+def add_product(db: Session, product: ProductBase):
+    db_product = db.add(Product(**product.model_dump()))
+    db.commit()
+    db.refresh(db_product)
+    return db_product
+
+def update_product( db:Session, id: int, product: ProductBase):
+    db_product = db.get(Product, id)
+    if db_product:
+        db_product.name = product.name
+        db_product.description = product.description
+        db_product.price = product.price
+        db_product.quantity = product.quantity
+        db.commit()
+        db.refresh(db_product)
+        return "Product Updated"
+
+    return False     
+
+def delete_product( db:Session, id: int):                            
+    db_product = db.get(Product, id)
+    if not db_product:
+        return False
+    
+    db.delete(db_product)
+    db.commit()
+    return {"detail" : "Product Deleted"}
+
