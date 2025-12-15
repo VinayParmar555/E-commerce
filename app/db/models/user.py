1d84f87f758eb9e0efc2af9a89a2b2859dd9892e
VinayParmar555 <vinayparmar111111@gmail.com>
2025-12-15 19:32:58 +0530

    Created user DB model

--- app/db/models/user.py
+++ app/db/models/user.py
@@ -1,6 +1,6 @@
-from sqlalchemy import String, Integer, Column
+from sqlalchemy import String, Integer, Column, DateTime
 from app.db.base import Base
-
+from datetime import datetime, timezone
 
 class Users(Base):
 
@@ -9,4 +9,13 @@ class Users(Base):
     user_id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String, nullable=False)
     email = Column(String, nullable=False, unique=True)
+    is_active = Column(bool = True)
+    is_admin = Column(bool = False)
     password = Column(String, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    is_verified = Column(bool = False)
+    created_at = Column(DateTime(timezone=True), default=lambda : datetime.now(timezone.utc))
+    updated_at = Column(DateTime(timezone=True), default=lambda : datetime.now(timezone.utc), onupdate=lambda : datetime.now(timezone.utc))
+
+
+    
