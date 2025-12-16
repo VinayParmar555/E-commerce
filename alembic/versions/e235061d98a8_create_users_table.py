f3f0162b7f5e3572594bbe85de66914ba2fd3511
VinayParmar555 <vinayparmar111111@gmail.com>
2025-12-16 23:24:47 +0530

    Fixed updated_at : added auto update feature

--- alembic/versions/e235061d98a8_create_users_table.py
+++ alembic/versions/e235061d98a8_create_users_table.py
@@ -21,15 +21,24 @@ depends_on: Union[str, Sequence[str], None] = None
 def upgrade() -> None:
     op.create_table(
         "users",
-        sa.Column("id", sa.Integer, primary_key=True),
-        sa.Column("name", sa.VARCHAR, nullable=False),
-        sa.Column("email", sa.VARCHAR, unique=True, nullable=False),
-        sa.Column("is_active", sa.Boolean, server_default=sa.true(), nullable=False),
-        sa.Column("is_admin", sa.Boolean, server_default=sa.false(), nullable=False),
-        sa.Column("hashed_password", sa.String, nullable=False),
-        sa.Column("is_verified", sa.Boolean, server_default=sa.false(), nullable=False),
-        sa.Column("created_at", sa.DateTime(timezone=True), server_default=sa.func.now(), nullable=False),
-        sa.Column("updated_at", sa.DateTime(timezone=True), server_default=sa.func.now(), nullable=False)
+        sa.Column("id", sa.Integer(), primary_key=True),
+        sa.Column("name", sa.VARCHAR(), nullable=False),
+        sa.Column("email", sa.VARCHAR(), unique=True, nullable=False),
+        sa.Column("is_active", sa.Boolean(), server_default=sa.true(), nullable=False),
+        sa.Column("is_admin", sa.Boolean(), server_default=sa.false(), nullable=False),
+        sa.Column("hashed_password", sa.String(), nullable=False),
+        sa.Column("is_verified", sa.Boolean(), server_default=sa.false(), nullable=False),
+        sa.Column("created_at",
+                  sa.DateTime(timezone=True), 
+                  server_default=sa.func.now(), 
+                  nullable=False
+        ),
+        sa.Column("updated_at", 
+                  sa.DateTime(timezone=True), 
+                  server_default=sa.func.now(),
+                  server_onupdate=sa.func.now(), 
+                  nullable=False
+        )
         
     )
 
