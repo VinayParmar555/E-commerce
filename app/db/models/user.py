1c83ad71c7927c0641bc5eb9cb5ac21f71e1c50b
VinayParmar555 <vinayparmar111111@gmail.com>
2025-12-17 16:45:06 +0530

    Updated user model with proper defaults and constraints

--- app/db/models/user.py
+++ app/db/models/user.py
@@ -1,19 +1,26 @@
-from sqlalchemy import String, Integer, Column, DateTime
+from sqlalchemy import String, Integer, Column, DateTime, Boolean, func
 from app.db.base import Base
-from datetime import datetime, timezone
 
 class Users(Base):
 
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, nullable=False)
+    id = Column(Integer, primary_key=True, index=True, nullable=False)
     name = Column(String, nullable=False)
-    email = Column(String, nullable=False, unique=True)
-    is_active = Column(bool = True)
-    is_admin = Column(bool = False)
-    password = Column(String, nullable=False)
+    email = Column(String, unique=True,index=True, nullable=False)
+    is_active = Column(Boolean, server_default=func.true(), nullable=False)
+    is_admin = Column(Boolean, server_default=func.false(), nullable=False)
     hashed_password = Column(String, nullable=False)
-    is_verified = Column(bool = False)
-    created_at = Column(DateTime(timezone=True), default=lambda : datetime.now(timezone.utc))
-    updated_at = Column(DateTime(timezone=True), default=lambda : datetime.now(timezone.utc), onupdate=lambda : datetime.now(timezone.utc))
+    is_verified = Column(Boolean, server_default=func.false(), nullable=False)
+    created_at = Column(
+        DateTime(timezone=True), 
+        server_default=func.now(), 
+        nullable=False
+    )
+    updated_at = Column(
+        DateTime(timezone=True), 
+        server_default=func.now(), 
+        server_onupdate=func.now(), 
+        nullable=False
+    )
 
