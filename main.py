13b16ae418ba2e1d4752d65104b6fa509c3834a7
VinayParmar555 <vinayparmar111111@gmail.com>
2025-12-18 21:01:24 +0530

    add auth routes to main application

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from fastapi import FastAPI
 from app.router.products import router as product_router
-# from fastapi import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-
-
+from app.router.users import router as users_router
 
 app = FastAPI(title="Radha Krishna")
 app.include_router(product_router)
-
+app.include_router(users_router)
