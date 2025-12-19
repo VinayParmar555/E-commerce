2bc948f392d8061e01a4e43b9122c29df9ed1904
VinayParmar555 <vinayparmar111111@gmail.com>
2025-12-19 21:50:34 +0530

    add authentication logic and token creation

--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -1,7 +1,12 @@
 from sqlalchemy.orm import Session
 from app.schema.user import UserCreate
 from app.db.models.user import Users
-from app.utils.hashing import hash_password
+from app.db.models.refresh_token import RefreshToken
+from app.utils.hashing import hash_password, verify_password
+from app.utils.jwt_manager import create_access_token
+from datetime import datetime, timedelta, timezone
+from app.config.settings import settings
+import uuid
 
 def create_user(db:Session, user: UserCreate):
     check_existing_user = db.query(Users).filter(Users.email == user.email).first()
@@ -18,10 +23,26 @@ def create_user(db:Session, user: UserCreate):
     db.refresh(new_user)
     return new_user
 
-# def delete_user(db: Session, email : str):
-#     db_user = db.query(Users).filter(Users.email == email).first()
-#     if not db_user:
-#         return False
-#     db.delete(db_user)
-#     db.commit()
-#     return {"detail" : "User Deleted"}
+def authenticate_user(db: Session, email: str, password: str):
+    db_user = db.query(Users).filter(Users.email == email).first()
+    if not db_user or not verify_password(password, db_user.hashed_password):
+        return None
+    return db_user
+
+def create_tokens(db: Session, user: Users):
+    access_token = create_access_token(data={"sub" : str(user.id)})
+    refresh_token_str = str(uuid.uuid4())
+    expires = datetime.now(timezone.utc) + timedelta(days=settings.REFRESH_TOKEN_EXPIRE_DAYS)
+    refresh_token = RefreshToken(
+        user_id = user.id,
+        token = refresh_token_str,
+        expires_at = expires
+    )
+    db.add(refresh_token)
+    db.commit()
+    db.refresh(refresh_token)
+    return{
+        "access_token" : access_token,
+        "refresh_token" : refresh_token_str,
+        "token" : "bearer"
+    }
