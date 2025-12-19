3fe9ec44bf25c8e32c2e8be6c90a84644c6f583f
VinayParmar555 <vinayparmar111111@gmail.com>
2025-12-19 21:43:04 +0530

    add created_at timestamp for refresh_tokens

--- alembic/versions/e21bef9cce55_create_refresh_tokens_table.py
+++ alembic/versions/e21bef9cce55_create_refresh_tokens_table.py
@@ -27,6 +27,7 @@ def upgrade() -> None:
         sa.Column(
             "created_at", 
             sa.DateTime(timezone=True), 
+            server_default=sa.func.now(),
             nullable=False
         ),
         sa.Column(
