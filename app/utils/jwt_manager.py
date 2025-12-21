f25adbb0f88f86008cd6ace62ff959ebefee785e
VinayParmar555 <vinayparmar111111@gmail.com>
2025-12-21 23:29:54 +0530

    add email verification token generation using JWT

--- app/utils/jwt_manager.py
+++ app/utils/jwt_manager.py
@@ -23,3 +23,8 @@ def decode_token(token: str):
         return payload
     except JWTError:
         return None
+    
+def create_email_verification_token(user_id: int):
+    expires = datetime.now(timezone.utc) + timedelta(minutes=settings.EMAIL_TOKEN_EXPIRE_MINUTES)
+    to_encode = {"sub" : str(user_id), "type":"verify", "exp":expires}
+    return jwt.encode(to_encode, settings.JWT_EMAIL_SECRET_KEY, algorithm=settings.JWT_ALGORITHM)
