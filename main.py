6f4d71dae5bd8ff4311e8664c8f93793c02438ba
VinayParmar555 <vinayparmar111111@gmail.com>
2025-12-22 12:40:46 +0530

    add user router to main app

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 from app.router.products import router as product_router
-from app.router.users import router as users_router
+from app.router.auth import router as auth_router
+from app.router.user import router as user_router
 
 app = FastAPI(title="Radha Krishna")
 app.include_router(product_router)
-app.include_router(users_router)
+app.include_router(auth_router)
+app.include_router(user_router)
