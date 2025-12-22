65314614b8bda9a34c312322b4a7423d78afa24d
VinayParmar555 <vinayparmar111111@gmail.com>
2025-12-22 15:01:17 +0530

    add email token decoding logic using JWT

--- app/utils/jwt_manager.py
+++ app/utils/jwt_manager.py
@@ -28,3 +28,9 @@ def create_email_verification_token(user_id: int):
     expires = datetime.now(timezone.utc) + timedelta(minutes=settings.EMAIL_TOKEN_EXPIRE_MINUTES)
     to_encode = {"sub" : str(user_id), "type":"verify", "exp":expires}
     return jwt.encode(to_encode, settings.JWT_EMAIL_SECRET_KEY, algorithm=settings.JWT_ALGORITHM)
+
+def verify_token(token: str):
+    payload = jwt.decode(token, settings.JWT_EMAIL_SECRET_KEY, algorithms=[settings.JWT_ALGORITHM])
+    if not payload:
+        return False
+    return {"msg" : "Email verified successfully"}
