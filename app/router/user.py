51575b763e037bca767b9e44fa96f140d6dd205c
VinayParmar555 <vinayparmar111111@gmail.com>
2025-12-23 23:57:41 +0530

    add endpoint for reset link

--- app/router/user.py
+++ app/router/user.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 from app.deps.db import get_db
 from app.schema.user import UserOut
 from app.deps.auth import get_current_user
-from app.services.user_service import change_password_process
+from app.services.user_service import change_password_process, reset_password_process
 from app.db.models.user import Users
 
 router = APIRouter(prefix="/profile", tags=["Profile"])
@@ -18,3 +18,10 @@ def change_password(old_password: str, new_password: str, db: Session = Depends(
     if not changed_password:
         raise HTTPException(status_code=404, detail="Invalid password")
     return changed_password
+
+@router.get("/reset-link")
+def reset_link(email: str, db: Session = Depends(get_db)):
+    result = reset_password_process(db, email)
+    if not result:
+        raise HTTPException(status_code=404, detail="Invalid email")
+    return result
