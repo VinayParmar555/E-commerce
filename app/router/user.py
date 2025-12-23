41502135273948e94c2fa41177cb9072ae223318
VinayParmar555 <vinayparmar111111@gmail.com>
2025-12-23 10:44:15 +0530

    add endpoint to change user password

--- app/router/user.py
+++ app/router/user.py
@@ -1,9 +1,20 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter,HTTPException, Depends
+from sqlalchemy.orm import Session
+from app.db.session import get_db
 from app.schema.user import UserOut
 from app.services.auth_service import get_current_user
+from app.services.user_service import change_password_process
+from app.db.models.user import Users
 
 router = APIRouter(prefix="/profile", tags=["View Profile"])
 
 @router.get("/me", response_model=UserOut)
 def me(user = Depends(get_current_user)):
     return user
+
+@router.post("/change-password")
+def change_password(old_password: str, new_password: str, db: Session = Depends(get_db), user:Users = Depends(get_current_user)):
+    changed_password = change_password_process(db, user, old_password, new_password)
+    if not changed_password:
+        raise HTTPException(status_code=404, detail="Invalid password")
+    return changed_password
