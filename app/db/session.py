dec387350fbcf02b22eb3e0518e324fe8067296c
VinayParmar555 <vinayparmar111111@gmail.com>
2025-12-23 18:50:07 +0530

    move get_db dependency to deps/db

--- app/db/session.py
+++ app/db/session.py
@@ -4,13 +4,4 @@ from app.config.settings import settings
 
 engine = create_engine(settings.DATABASE_URL)
 
-session = sessionmaker(autocommit = False,autoflush=False,bind=engine)
-
-#Database dependency
-def get_db():
-    db = session()
-    try:
-        yield db
-    finally:
-        db.close()
-
+session = sessionmaker(autocommit = False, autoflush=False, bind=engine)
