c3d66c0782da67598523e62ce9d7cc7aa430b789
VinayParmar555 <vinayparmar111111@gmail.com>
2025-12-23 16:11:06 +0530

    Renamed title

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from app.router.products import router as product_router
 from app.router.auth import router as auth_router
 from app.router.user import router as user_router
 
-app = FastAPI(title="Radha Krishna")
+app = FastAPI(title="E-commerce Website")
 app.include_router(product_router)
 app.include_router(auth_router)
 app.include_router(user_router)
