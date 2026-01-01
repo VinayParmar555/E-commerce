1f9dd6b12c2ed73f690d24c857f4d431872be9f1
VinayParmar555 <vinayparmar111111@gmail.com>
2026-01-01 10:55:13 +0530

    fix routes endpoint to be asynchronous

--- app/router/user.py
+++ app/router/user.py
@@ -16,25 +16,25 @@ from app.db.models.user import Users
 router = APIRouter(prefix="/profile", tags=["Profile"])
 
 @router.get("/me", response_model=UserOut)
-def me(user = Depends(get_current_user)):
+async def me(user = Depends(get_current_user)):
     return user
 
 @router.put("/change-password")
-def change_password(old_password: str, new_password: str, db: Session = Depends(get_db), user:Users = Depends(get_current_user)):
+async def change_password(old_password: str, new_password: str, db: Session = Depends(get_db), user:Users = Depends(get_current_user)):
     result = change_password_process(db, user, old_password, new_password)
     if not result:
         raise HTTPException(status_code=400, detail="Incorrect old password")
     return result
 
 @router.post("/forgot-password")
-def forgot_password(email: str, db: Session = Depends(get_db)):
+async def forgot_password(email: str, db: Session = Depends(get_db)):
     result = reset_password_process(db, email)
     if not result:
         raise HTTPException(status_code=400, detail="Email not registered")
     return result
 
 @router.post("/set-password")
-def set_new_password(new_password:str, token:str, db: Session = Depends(get_db)):
+async def set_new_password(new_password:str, token:str, db: Session = Depends(get_db)):
     result = verify_rtoken(db, token, new_password)
     if result is False:
         raise HTTPException(status_code=401, detail="Invalid or expired token")
@@ -43,7 +43,7 @@ def set_new_password(new_password:str, token:str, db: Session = Depends(get_db))
     return result
 
 @router.post("/make-admin")
-def make_admin(user_id: int, db: Session = Depends(get_db), current_user: Users =Depends(get_current_user)):
+async def make_admin(user_id: int, db: Session = Depends(get_db), current_user: Users =Depends(get_current_user)):
     if not current_user.is_admin:
         raise HTTPException(status_code=403, detail="Admin access required")
     result = promote_admin(db, user_id)
@@ -54,7 +54,7 @@ def make_admin(user_id: int, db: Session = Depends(get_db), current_user: Users 
     return result
 
 @router.post("/logout")
-def logout(request: Request, db:Session = Depends(get_db)):
+async def logout(request: Request, db:Session = Depends(get_db)):
     token = request.cookies.get("refresh_token")
     if not token:
         raise HTTPException(status_code=401, detail="not logged in")
