fd0b59207ce112c0d07fe58359a8d4b11dac0925
VinayParmar555 <vinayparmar111111@gmail.com>
2026-01-05 23:26:06 +0530

    add model for category

---
+++ app/db/models/category.py
@@ -0,0 +1,12 @@
+from sqlalchemy.orm import relationship
+from sqlalchemy import String, Integer, Column
+from app.db.base import Base
+
+class Category(Base):
+    
+    __tablename__ = "categories"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+
+    products = relationship("Product", back_populates="category")
