a99bdd2674c5ddf9f4851c9206219890a50fc534
VinayParmar555 <vinayparmar111111@gmail.com>
2026-01-07 23:55:10 +0530

    integrate category router into main application

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from fastapi import FastAPI
 from app.router.products import router as product_router
 from app.router.auth import router as auth_router
 from app.router.user import router as user_router
+from app.router.category import router as category_router
 
 app = FastAPI(title="E-commerce Website")
 app.include_router(product_router)
 app.include_router(auth_router)
 app.include_router(user_router)
+app.include_router(category_router)
