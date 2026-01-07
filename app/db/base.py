0587302314989a35ff10946432e8574431f380f7
VinayParmar555 <vinayparmar111111@gmail.com>
2026-01-08 00:23:22 +0530

    initialize models after Base for table relationships

--- app/db/base.py
+++ app/db/base.py
@@ -1,3 +1,7 @@
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
+
+from app.db.models.cart import Cart
+from app.db.models.products import Product
+from app.db.models.category import Category
