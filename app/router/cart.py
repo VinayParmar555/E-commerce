95d410a2359b66e2cdf29c4a29b24914a73eea2d
VinayParmar555 <vinayparmar111111@gmail.com>
2026-01-19 19:33:27 +0530

    add add-to-cart endpoint

---
+++ app/router/cart.py
@@ -0,0 +1,14 @@
+from fastapi import HTTPException, Depends, APIRouter
+from sqlalchemy.orm import Session
+from app.deps.db import get_db
+from app.schema.cart import CartOut
+from app.services.cart_service import add_to_cart
+
+router = APIRouter(prefix="/Cart", tags=["Cart"])
+
+@router.post("/add_cart")
+async def add_in_cart(cart_item:CartOut, db:Session=Depends(get_db)):
+    cart = add_to_cart(db, cart_item)
+    if not cart:
+        raise HTTPException(status_code=404, detail="Insufficient stock")
+    return cart
