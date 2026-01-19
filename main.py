5dd705e0574134eeba7b6098042ded2466113a8c
VinayParmar555 <vinayparmar111111@gmail.com>
2026-01-19 18:47:00 +0530

    add cart router in main application

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ from app.router.products import router as product_router
 from app.router.auth import router as auth_router
 from app.router.user import router as user_router
 from app.router.category import router as category_router
+from app.router.cart import router as cart_router
 
 app = FastAPI(title="E-commerce Website")
 app.include_router(product_router)
 app.include_router(auth_router)
 app.include_router(user_router)
 app.include_router(category_router)
+app.include_router(cart_router)
