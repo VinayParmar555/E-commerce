3bddf5458a90f6aaeb5d0688263977033fa948db
VinayParmar555 <vinayparmar111111@gmail.com>
2026-01-19 19:28:53 +0530

    import cart model

--- app/db/base.py
+++ app/db/base.py
@@ -2,6 +2,6 @@ from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
 
-# from app.db.models.cart import Cart
+from app.db.models.cart import Cart
 from app.db.models.products import Product
 from app.db.models.category import Category
