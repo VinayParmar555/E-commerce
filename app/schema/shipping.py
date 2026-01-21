99682c3fe7b4bd228b5841b24b4e53c08b1ffc41
VinayParmar555 <vinayparmar111111@gmail.com>
2026-01-21 18:27:03 +0530

    fix schema for shipping

--- app/schema/shipping.py
+++ app/schema/shipping.py
@@ -1,11 +1,13 @@
 from pydantic import BaseModel
 
-class ShippingAddress(BaseModel):
-    id:int
-    user_id:int
+class ShippingBase(BaseModel):
     address_line1:str
     address_line2:str | None = None
     city:str
     postal_code:int
     state:str
     country:str
+
+class ShippingAddress(ShippingBase):
+    id:int
+    user_id:int
