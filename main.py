e8b70f01f7c507dc0357341bcb3ca66e6180f516
VinayParmar555 <vinayparmar111111@gmail.com>
2026-01-21 18:15:10 +0530

    Add shipping address router in main application

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from app.router.auth import router as auth_router
 from app.router.user import router as user_router
 from app.router.category import router as category_router
 from app.router.cart import router as cart_router
+from app.router.shipping import router as shipping_router
 
 app = FastAPI(title="E-commerce Website")
 app.include_router(product_router)
@@ -11,3 +12,4 @@ app.include_router(auth_router)
 app.include_router(user_router)
 app.include_router(category_router)
 app.include_router(cart_router)
+app.include_router(shipping_router)
