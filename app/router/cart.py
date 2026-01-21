f77bf32cd3d255f973c999e483ac6dd823f599ed
VinayParmar555 <vinayparmar111111@gmail.com>
2026-01-21 16:07:08 +0530

    add endpoint for remove_cart and fix error response

--- app/router/cart.py
+++ app/router/cart.py
@@ -2,9 +2,9 @@ from fastapi import HTTPException, Depends, APIRouter
 from sqlalchemy.orm import Session
 from app.deps.db import get_db
 from app.deps.auth import get_current_user
-from app.schema.cart import CartOut, CartItem
+from app.schema.cart import CartItem
 from app.db.models.user import Users
-from app.services.cart_service import add_to_cart, see_cart
+from app.services.cart_service import add_to_cart, remove_cart, see_cart
 
 router = APIRouter(prefix="/Cart", tags=["Cart"])
 
@@ -21,5 +21,12 @@ async def add_in_cart(cart_item:CartItem, user:Users=Depends(get_current_user), 
 async def check_cart(user:Users=Depends(get_current_user), db:Session=Depends(get_db)):
     cart = see_cart(db, user.id)
     if not cart:
-        raise HTTPException(status_code=404, detail="user not found")
+        raise HTTPException(status_code=404, detail="add items to see cart")
     return cart
+
+@router.delete("/delete_cart/{cart_id}")
+async def delete_cart(cart_id:int, user:Users=Depends(get_current_user), db:Session=Depends(get_db)):
+    cart = remove_cart(db, user.id, cart_id)
+    if not cart:
+        raise HTTPException(status_code=404, detail="cart not found")
+    return {"msg":"cart deleted succesfully"}
