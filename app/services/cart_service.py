01aaa3f10b70145184bf859e37fecc85776dc19d
VinayParmar555 <vinayparmar111111@gmail.com>
2026-01-23 13:52:59 +0530

    fix double query

--- app/services/cart_service.py
+++ app/services/cart_service.py
@@ -9,10 +9,10 @@ def add_to_cart(db:Session, cart_item:CartItem, user_id:int):
     user = db.get(Users, user_id)
     if not user:
         return False
-    product = db.query(Product).filter(Product.id==cart_item.product_id).with_for_update().first()
+    product = db.query(Product).filter(Product.id==cart_item.product_id).first()
     if not product or product.quantity<cart_item.quantity:
         return None
-    stmt = db.query(Cart).filter(Cart.user_id==user_id, Cart.product_id==cart_item.product_id).with_for_update().first()
+    stmt = db.query(Cart).filter(Cart.user_id==user_id, Cart.product_id==cart_item.product_id).first()
     if stmt:
         stmt.quantity+=cart_item.quantity
         stmt.total_price=product.price*stmt.quantity
@@ -28,7 +28,7 @@ def see_cart(db:Session, user_id:int):
     items = db.query(Cart).filter(Cart.user_id==user_id).all()
     if not items:
         return None
-    total_price = db.query(func.sum(Cart.total_price)).filter(Cart.user_id==user_id).scalar()
+    total_price = sum(item.total_price for item in items)
     return {"items" : items, "total_price" : total_price}
 
 def remove_cart(db:Session, user_id:int, cart_id:int):
