9e81d67634b4a531b82dd0ff68dbffa8be28c3aa
VinayParmar555 <vinayparmar111111@gmail.com>
2026-01-23 13:51:58 +0530

    import models for ORM registry

--- app/db/base.py
+++ app/db/base.py
@@ -5,3 +5,6 @@ Base = declarative_base()
 from app.db.models.cart import Cart
 from app.db.models.products import Product
 from app.db.models.category import Category
+from app.db.models.order import Order, OrderItem
+from app.db.models.payment import Payment
+from app.db.models.shipping import ShippingAddress, ShippingStatus
