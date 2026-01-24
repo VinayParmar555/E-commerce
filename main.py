965886e518f0b76aef81921cb91d201203e146e9
VinayParmar555 <vinayparmar111111@gmail.com>
2026-01-24 19:49:12 +0530

    include order routes in main application

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from app.router.user import router as user_router
 from app.router.category import router as category_router
 from app.router.cart import router as cart_router
 from app.router.shipping import router as shipping_router
+from app.router.order import router as order_router
 
 app = FastAPI(title="E-commerce Website")
 app.include_router(product_router)
@@ -13,3 +14,4 @@ app.include_router(user_router)
 app.include_router(category_router)
 app.include_router(cart_router)
 app.include_router(shipping_router)
+app.include_router(order_router)
