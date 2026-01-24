edb12035d2a312e5dc42a31183edfee0b4e91bc9
VinayParmar555 <vinayparmar111111@gmail.com>
2026-01-24 19:25:31 +0530

    add schema for payment status and gateway

--- app/schema/payment.py
+++ app/schema/payment.py
@@ -1,4 +1,6 @@
+from datetime import datetime
 from enum import Enum
+from pydantic import BaseModel
 
 class PaymentStatus(str, Enum):
     pending = "pending"
@@ -9,3 +11,9 @@ class PaymentStatus(str, Enum):
 class PaymentGateway(Enum):
     mock = "mock"
     razorpay = "razorpay"
+
+class PaymentCreate(BaseModel):
+    amount : int
+    shipping_address_id : int
+    gateway : PaymentGateway = PaymentGateway.mock
+    simulate_succ : bool | None = None
