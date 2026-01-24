3e35408030fdb6f44cd8e3ce19c893ec99208ba4
VinayParmar555 <vinayparmar111111@gmail.com>
2026-01-24 19:24:55 +0530

    add schema for order(items and status)

--- app/schema/order.py
+++ app/schema/order.py
@@ -1,11 +1,20 @@
 from datetime import datetime
 from enum import Enum
 from pydantic import BaseModel
+from app.schema.shipping import ShippingStatusResponse, ShippingAddress
 
 class OrderStatus(str, Enum):
     pending = "pending"
     confirmed = "confirmed"
     cancelled = "cancelled"
+
+class OrderItem(BaseModel): 
+    order_id : int
+    product_id : int
+    quantity : int
+    price : int
+
+    model_config = {"from_attributes" : True}
 
 class Order(BaseModel):
     id : int
@@ -14,3 +23,8 @@ class Order(BaseModel):
     total_price : float
     status : OrderStatus
     created_at : datetime
+    items : list[OrderItem]
+    shippingstatus : list[ShippingStatusResponse]
+    shippingaddress : ShippingAddress
+
+    model_config = {"from_attributes" : True}
