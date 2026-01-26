a71dcebc83b886846968dc8234106c6f095b37d7
VinayParmar555 <vinayparmar111111@gmail.com>
2026-01-26 16:40:52 +0530

    add endpoint to fetch all payment status

--- app/router/payment.py
+++ app/router/payment.py
@@ -4,13 +4,20 @@ from app.deps.auth import get_current_user
 from app.deps.db import get_db
 from app.db.models.user import Users
 from app.schema.payment import PaymentResponse
-from app.services.payment_service import fetch_payment_status
+from app.services.payment_service import fetch_all_payments, fetch_payment_status
 
 router = APIRouter(prefix="/payment", tags=["Payment"])
 
 @router.patch("/payment_status/{order_id}", response_model=PaymentResponse)
 async def check_payment_status(order_id:int, user:Users=Depends(get_current_user), db:Session=Depends(get_db)):
     payment = fetch_payment_status(db, user.id, order_id)
+    if payment is None:
+        raise HTTPException(status_code=404, detail="Payment not found!")
+    return payment
+
+@router.patch("/payment_status_all/{order_id}")
+async def check_payment_status_all(user:Users=Depends(get_current_user), db:Session=Depends(get_db)):
+    payment = fetch_all_payments(db, user.id)
     if payment is None:
         raise HTTPException(status_code=404, detail="Payment not found!")
     return payment
