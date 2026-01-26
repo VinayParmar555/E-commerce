f0bbcf0012f0be369ddd70b2462404486f862ef8
VinayParmar555 <vinayparmar111111@gmail.com>
2026-01-26 16:28:04 +0530

    add schema for payment response

--- app/schema/payment.py
+++ app/schema/payment.py
@@ -17,3 +17,19 @@ class PaymentCreate(BaseModel):
     shipping_address_id : int
     gateway : PaymentGateway = PaymentGateway.mock
     simulate_succ : bool | None = None
+
+class PaymentResponse(BaseModel):
+    id : int
+    order_id : int
+    user_id : int
+    amount : int
+    status : PaymentStatus
+    payment_gateway : PaymentGateway
+    is_paid : bool
+    pg_order_id : str
+    pg_payment_id : str
+    pg_signature : str
+    created_at : datetime
+    updated_at : datetime
+
+    model_config = {"from_attributes" : True}
