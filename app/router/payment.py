fceb3138996d9e68f092665eb7e979adcfb7f78a
VinayParmar555 <vinayparmar111111@gmail.com>
2026-01-27 22:07:34 +0530

    fix endpoint

--- app/router/payment.py
+++ app/router/payment.py
@@ -8,14 +8,14 @@ from app.services.payment_service import fetch_all_payments, fetch_payment_statu
 
 router = APIRouter(prefix="/payment", tags=["Payment"])
 
-@router.patch("/payment_status/{order_id}", response_model=PaymentResponse)
+@router.patch("/status/{order_id}", response_model=PaymentResponse)
 async def check_payment_status(order_id:int, user:Users=Depends(get_current_user), db:Session=Depends(get_db)):
     payment = fetch_payment_status(db, user.id, order_id)
     if payment is None:
         raise HTTPException(status_code=404, detail="Payment not found!")
     return payment
 
-@router.patch("/payment_status_all", response_model=list[PaymentResponse])
+@router.patch("/status/all", response_model=list[PaymentResponse])
 async def check_payment_status_all(user:Users=Depends(get_current_user), db:Session=Depends(get_db)):
     payment = fetch_all_payments(db, user.id)
     if payment is None:
