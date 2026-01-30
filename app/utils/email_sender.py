5ae09e03839c4127967cd2b6d8b88547b86831fc
VinayParmar555 <vinayparmar111111@gmail.com>
2026-01-30 23:37:34 +0530

    integrate sendgrid for automated email sending

--- app/utils/email_sender.py
+++ app/utils/email_sender.py
@@ -1,15 +1,14 @@
-import smtplib
-from email.message import EmailMessage
 from app.config.settings import settings
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
 
 def send_email(to_email:str, subject:str, body:str):
-    msg = EmailMessage()
-    msg["From"] = settings.SMTP_EMAIL
-    msg["To"] = to_email
-    msg["Subject"] = subject
-    msg.set_content(body)
+    msg = Mail(
+        from_email=settings.FROM_EMAIL,
+        to_emails=to_email,
+        subject=subject,
+        plain_text_content=body
+    )
 
-    with smtplib.SMTP(settings.SMTP_SERVER, settings.SMTP_PORT) as server:
-        server.starttls()
-        server.login(settings.SMTP_EMAIL, settings.SMTP_PASSWORD)
-        server.send_message(msg)
+    sg = SendGridAPIClient(settings.SENDGRID_API_KEY)
+    sg.send(msg)
