21d60fc2d549b5681460f239f705900d8b16cc27
VinayParmar555 <vinayparmar111111@gmail.com>
2026-01-31 20:22:04 +0530

    add API integration tests using pytest

--- test/test_main.py
+++ test/test_main.py
@@ -3,16 +3,45 @@ from main import app
 
 client = TestClient(app)
 
-def test_login():
-    
-    r1 = client.post("/account/login", data={
-        "username" : "rukmini@example.com",
-        "password" : "rukmini"
-    })
-    assert r1.status_code == 200
-    return r1.json()["access_token"]
+def register():
+    payload={
+        "name" : "demo",
+        "email" : "demo@example.com",
+        "password" : "demo123"
+    }
+    return client.post("/account/register", json=payload)
 
-def test_me():
-    token = test_login()
-    r2 = client.get("/profile/me", headers={"Authorization": f"Bearer {token}"})
-    assert r2.status_code == 200
+def login():
+     payload={
+        "username" : "demo@example.com",
+        "password" : "demo123"
+    }
+     return client.post("/account/login", data=payload)
+
+def test_register():
+    response = register()
+    assert response.status_code==200
+    data = response.json()
+    assert data["email"] == "demo@example.com"
+    assert "id" in data
+    assert "password" not in data
+
+def test_register_duplicate():
+    payload={
+        "name" : "demo",
+        "email" : "demo@example.com",
+        "password" : "demo123"
+    }
+    response = client.post("/account/register", json=payload)
+    assert response.status_code==400
+
+def test_login():
+    response = login()
+    assert response.status_code == 200
+    return response.json()["access_token"]
+
+def test_verify_request():
+    fetch_token = login()
+    token = fetch_token.json()["access_token"]
+    response = client.post("/account/verify-request", headers={"Authorization" : f"Bearer {token}"})
+    assert response.status_code==200
