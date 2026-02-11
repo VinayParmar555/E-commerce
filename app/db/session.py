8f68f4c2f96782645e9a43000e36382d61729f8e
VinayParmar555 <vinayparmar111111@gmail.com>
2026-02-11 19:16:40 +0530

    force IPv4 connection for Supabase on Render

--- app/db/session.py
+++ app/db/session.py
@@ -2,6 +2,17 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from app.config.settings import settings
 
-engine = create_engine(settings.DATABASE_URL)
+connect_args = {
+    "sslmode": "require"
+}
+
+hostaddr = settings.DATABASE_HOSTADDR
+if hostaddr:
+    connect_args["hostaddr"] = hostaddr
+
+engine = create_engine(
+    settings.DATABASE_URL,
+    connect_args=connect_args
+)
 
 session = sessionmaker(autocommit = False, autoflush=False, bind=engine)
